868b7b37ba8731048b57887ca34d79820ea46841
eaststrong <eaststrongking@gmail.com>
2016-09-17 12:09:12 +0900

    eclipse

--- src/main/java/com/minitwit/App.java
+++ src/main/java/com/minitwit/App.java
@@ -1,30 +1,31 @@
 package com.minitwit;
 
+import static spark.Spark.*;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import static spark.Spark.*;
 import com.minitwit.config.WebConfig;
 import com.minitwit.service.impl.MiniTwitService;
 
 @Configuration
 @ComponentScan({ "com.minitwit" })
 public class App {
-	public static void main(String[] args) {
-		port(getHerokuAssignedPort());
-    	AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(App.class);
-    	new WebConfig(ctx.getBean(MiniTwitService.class));
-        ctx.registerShutdownHook();
+  public static void main(String[] args) {
+    port(getHerokuAssignedPort());
+    @SuppressWarnings("resource")
+    AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(App.class);
+    new WebConfig(ctx.getBean(MiniTwitService.class));
+    ctx.registerShutdownHook();
+  }
+
+  static int getHerokuAssignedPort() {
+    ProcessBuilder processBuilder = new ProcessBuilder();
+
+    if (processBuilder.environment().get("PORT") != null) {
+      return Integer.parseInt(processBuilder.environment().get("PORT"));
     }
 
-    static int getHerokuAssignedPort() {
-        ProcessBuilder processBuilder = new ProcessBuilder();
-
-        if (processBuilder.environment().get("PORT") != null) {
-            return Integer.parseInt(processBuilder.environment().get("PORT"));
-        }
-
-        return 4567;
-    }
+    return 4567;
+  }
 }
