d16f5c34fdb487ea3f2307c4f7e323ec05df2da5
eaststrong <eaststrongking@gmail.com>
2016-09-17 10:57:07 +0900

    heroku

--- README.md
+++ README.md
@@ -18,6 +18,16 @@ Java version of Flask's MiniTwit example built with the Spark web microframework
 
 4. Log in as user001 with password user001, or user002/user002, or user003/user003 until user010/user010, or sign up yourself. If your e-mail address has an associated Gravatar image, this will be used as your profile image.
 
+##How to deploy it on heroku
+
+1. Clone the repository and go to the root directory.
+
+2. Execute `heroku create esktwit && mvn heroku:deploy`
+
+3. Open in your browser `http://esktwit.herokuapp.com/`
+
+4. Log in as user001 with password user001, or user002/user002, or user003/user003 until user010/user010, or sign up yourself. If your e-mail address has an associated Gravatar image, this will be used as your profile image.
+
 ##License
 MIT License
 
--- src/main/java/com/minitwit/App.java
+++ src/main/java/com/minitwit/App.java
@@ -4,18 +4,27 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import static spark.Spark.*;
 import com.minitwit.config.WebConfig;
 import com.minitwit.service.impl.MiniTwitService;
 
 @Configuration
 @ComponentScan({ "com.minitwit" })
 public class App {
-	
 	public static void main(String[] args) {
+		port(getHerokuAssignedPort());
     	AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(App.class);
     	new WebConfig(ctx.getBean(MiniTwitService.class));
         ctx.registerShutdownHook();
     }
-    
-    
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+
+        return 4567;
+    }
 }
